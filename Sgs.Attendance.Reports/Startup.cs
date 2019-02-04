edfa60151f0ae078e9cb65b40399a83a313febab
Sameer Al Harbi <smrhrbi@gmail.com>
2019-02-05 01:47:34 +0300

    Initial Project

--- Sgs.Attendance.Reports/Startup.cs
+++ Sgs.Attendance.Reports/Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+//using Sameer.Shared.Helpers.Mvc;
+using System.Globalization;
 
 namespace Sgs.Attendance.Reports
 {
@@ -15,20 +13,31 @@ namespace Sgs.Attendance.Reports
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error/500");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+
+            //app.UseNodeModules(env.ContentRootPath);
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
---
+++ Sgs.Attendance.Reports/wwwroot/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    $(function () {
+
+        //animate numbers
+        $('[data-animate-number]').each(function () {
+
+            var $this = $(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2900,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+
+        });
+
+    });
+
+})();
