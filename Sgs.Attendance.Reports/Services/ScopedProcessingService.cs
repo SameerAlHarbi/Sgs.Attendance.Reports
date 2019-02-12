5e0dabc17b9bb673a6cb365e7670c4657655fc17
Sameer Al Harbi <smrhrbi@gmail.com>
2019-02-13 01:43:12 +0300

    still processing

--- Sgs.Attendance.Reports/Services/ScopedProcessingService.cs
+++ Sgs.Attendance.Reports/Services/ScopedProcessingService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Sameer.Shared;
 using Sgs.Attendance.Reports.Logic;
+using Sgs.Attendance.Reports.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,25 +15,43 @@ namespace Sgs.Attendance.Reports.Services
         private readonly ProcessingsRequestsManager _processingsRequestsManager;
         private readonly EmployeesDaysReportsManager _employeesDaysReportsManager;
         private readonly IErpManager _erpManager;
+        private readonly EmployeesCalendarsManager _employeesCalendarManager;
+        private readonly EmployeesExcusesManager _employeesExcusesManager;
         private readonly ILogger _logger;
 
         public ScopedProcessingService(
               ProcessingsRequestsManager processingsRequestsManager
             , EmployeesDaysReportsManager employeesDaysReportsManager
             , IErpManager erpManager
+            , EmployeesCalendarsManager employeesCalendarManager
+            , EmployeesExcusesManager employeesExcusesManager
             , ILogger<ScopedProcessingService> logger)
         {
             _processingsRequestsManager = processingsRequestsManager;
             _employeesDaysReportsManager = employeesDaysReportsManager;
             _erpManager = erpManager;
+            _employeesCalendarManager = employeesCalendarManager;
+            _employeesExcusesManager = employeesExcusesManager;
             _logger = logger;
         }
 
-        private async Task<bool> processDaysReports(IEnumerable<int> employeesIds ,DateTime fromDate,DateTime toDate)
+        private async Task<bool> processDaysReports(IEnumerable<ShortEmployeeInfoViewModel> employees ,DateTime fromDate,DateTime toDate)
         {
             try
             {
-                
+                //Prepare employees data
+                var employeesIds = employees.Select(e => e.EmployeeId).OrderBy(e => e).ToList();
+
+                //Get employees Calendars
+                var allEmployeesCalendars = await _employeesCalendarManager.GetAllAsNoTrackingListAsync(ec => employeesIds.Contains(ec.EmployeeId)
+                    && ec.EndDate == null || (ec.StartDate >= fromDate && ec.StartDate <= toDate) || (ec.StartDate <= fromDate && ec.EndDate >= fromDate));
+
+                //Get employees excuses
+                var allEmployeesExcuses = await _employeesExcusesManager.GetAllAsNoTrackingListAsync(ex => employeesIds.Contains(ex.EmployeeId)
+                    && ex.ExcueseDate >= fromDate && ex.ExcueseDate <= toDate);
+
+
+
                 return true;
             }
             catch (Exception ex)
@@ -60,29 +79,43 @@ namespace Sgs.Attendance.Reports.Services
 
                     int lastEmployeeId = lastDayReport?.EmployeeId ?? 0;
                     DateTime fromDate = lastDayReport?.DayDate.AddDays(1) ?? openProcessingRequest.FromDate;
-                    DateTime toDate = lastDayReport?.DayDate.AddDays(5) ?? openProcessingRequest.FromDate.AddDays(5);
+                    DateTime toDate = lastDayReport?.DayDate.AddDays(10) ?? openProcessingRequest.FromDate.AddDays(10);
                     toDate = toDate <= openProcessingRequest.ToDate ? toDate : openProcessingRequest.ToDate;
 
                     var employees = await _erpManager.GetShortEmployeesInfo(employeesIds);
                     var lastRequestEmployeeId = employees.Max(e => e.EmployeeId);
 
-                    if (fromDate > openProcessingRequest.ToDate)// && lastEmployeeId)
+                    if (fromDate > openProcessingRequest.ToDate)
                     {
-                        openProcessingRequest.Completed = true;
-                        await _processingsRequestsManager.UpdateItemAsync(openProcessingRequest);
-                        return true;
+                        if (lastEmployeeId >= lastRequestEmployeeId)
+                        {
+                            openProcessingRequest.Completed = true;
+                            await _processingsRequestsManager.UpdateItemAsync(openProcessingRequest);
+                            return true; 
+                        }
+
+                        fromDate = openProcessingRequest.FromDate;
+                        toDate = openProcessingRequest.FromDate.AddDays(10);
+                        toDate = toDate <= openProcessingRequest.ToDate ? toDate : openProcessingRequest.ToDate;
+
+                        employees = employees
+                        .Where(e => e.EmployeeId > lastEmployeeId).OrderBy(e => e.EmployeeId)
+                        .Take(100).ToList();
                     }
-
-
-                    //if (lastDayReport == null)
-                    //{
-                    //    employeesIds = employees.OrderBy(e => e.EmployeeId).Select(e => e.EmployeeId).Distinct().Take(10).ToList();
-                    //    return await processDaysReports(employeesIds, openProcessingRequest.FromDate, openProcessingRequest openProcessingRequest.ToDate);
-                    //}
-                    //else
-                    //{
-                    //    employeesIds = employees.Where().OrderBy(e => e.EmployeeId).Select(e => e.EmployeeId).Distinct().Take(10).ToList();
-                    //}  
+                    else if(lastEmployeeId > 0)
+                    {
+                        employees = employees
+                        .Where(e => e.EmployeeId <= lastEmployeeId).OrderBy(e => e.EmployeeId)
+                        .Take(100).ToList();
+                    }
+                    else
+                    {
+                        employees = employees
+                        .Where(e => e.EmployeeId > lastEmployeeId).OrderBy(e => e.EmployeeId)
+                        .Take(100).ToList();
+                    }
+               
+                    return await processDaysReports(employees, fromDate, toDate);
                 }
             }
             catch (Exception)
