5a04c6a2824c4186c0b86b93c115bad6045d918b
Sameer Al Harbi <smrhrbi@gmail.com>
2019-02-17 08:55:46 +0300

    WorkCalendars

--- Sgs.Attendance.Reports/Controllers/WorkCalendarsController.cs
+++ Sgs.Attendance.Reports/Controllers/WorkCalendarsController.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using Sgs.Attendance.Reports.Logic;
 using Sgs.Attendance.Reports.Models;
 using Sgs.Attendance.Reports.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Sgs.Attendance.Reports.Controllers
@@ -17,7 +19,7 @@ namespace Sgs.Attendance.Reports.Controllers
         {
         }
 
-        public virtual async Task<IActionResult> GetAllCalendarsByWorkTimeJsonForKendo([DataSourceRequest] DataSourceRequest request
+        public async Task<IActionResult> GetAllCalendarsByWorkTimeJsonForKendo([DataSourceRequest] DataSourceRequest request
            , ContractWorkTime contractWorkTime)
         {
             var manager = _dataManager as WorkCalendarsManager;
@@ -26,6 +28,37 @@ namespace Sgs.Attendance.Reports.Controllers
             var resultDataModelList = await mapToViewModelsDataCollection(resultDataList);
             resultDataModelList = await fillMissingItemsData(resultDataModelList);
             return Json(resultDataModelList.ToDataSourceResult(request));
+        }
+
+        public async Task<IActionResult> UpdateCalendarsInfo(WorkCalendarViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                }
+
+                var errors = new List<string>();
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+
+                return Json(new { errors = errors.ToArray() });
+                
+            }
+            catch(ValidationException ex)
+            {
+                return Json(new { errors = new string[] { ex.ValidationResult.ErrorMessage } });
+            }
+            catch (System.Exception)
+            {
+                return Json(new { errors = new string[] { "خطأ أثناء حفظ بيانات التقويم الرجاء المحاولة لاحقاً"} });
+            }
         }
 
     }
---
+++ Sgs.Attendance.Reports/Migrations/14400612050238_RemoveVacationsDescription.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sgs.Attendance.Reports.Migrations
+{
+    public partial class RemoveVacationsDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VacationDescription",
+                table: "WorkCalendars");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VacationDescription",
+                table: "WorkCalendars",
+                nullable: true);
+        }
+    }
+}
