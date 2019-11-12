fb778f1862c2fb1fe5dc289381e170122dcf74f3
Sameer Al Harbi <smrhrbi@gmail.com>
2019-11-12 08:31:25 +0300

    employee calendar

--- Sgs.Attendance.Reports/Logic/EmployeesCalendarsManager.cs
+++ Sgs.Attendance.Reports/Logic/EmployeesCalendarsManager.cs
@@ -1,4 +1,8 @@
-﻿using Sameer.Shared.Data;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Sameer.Shared.Data;
 using Sgs.Attendance.Reports.Models;
 
 namespace Sgs.Attendance.Reports.Logic
@@ -7,6 +11,29 @@ namespace Sgs.Attendance.Reports.Logic
     {
         public EmployeesCalendarsManager(IRepository repo) : base(repo)
         {
+        }
+
+        protected override async Task<ICollection<ValidationResult>> ValidateUniqueItemAsync(EmployeeCalendar newItem)
+        {
+            var results = await base.ValidateUniqueItemAsync(newItem);
+
+            var allEmployeeCalendars = await GetAllAsNoTrackingListAsync(c => c.EmployeeId == newItem.EmployeeId);
+
+            if(allEmployeeCalendars.Count > 0)
+            {
+                if (allEmployeeCalendars.Any(c => !newItem.EndDate.HasValue && c.StartDate == newItem.StartDate && c.Id != newItem.Id))
+                {
+                    results.Add(new ValidationResult("Calendar start date duplicate",new string[] { "StartDate" }));
+                }
+                else if (allEmployeeCalendars.Any(c => newItem.EndDate.HasValue 
+                    && (c.EndDate.HasValue && (c.StartDate >= newItem.StartDate && c.StartDate <= newItem.EndDate)
+                    || ( newItem.StartDate >= c.StartDate && newItem.StartDate <= c.EndDate))))
+                {
+
+                }
+            }
+
+            return results;
         }
     }
 }
